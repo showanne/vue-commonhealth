b15c5877d2b7233d240ecfd44a941d397fa3658e
Anne <anne@ks-design.com.tw>
2024-02-19 00:39:16 +0800

    Add：bootstrap

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/style/app.scss'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
 import 'swiper/css'

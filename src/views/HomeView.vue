cc1bdc6c5b00e66a937e2052a24384f88fd37bf1
Anne <anne@ks-design.com.tw>
2024-02-20 00:08:27 +0800

    Update： swiper effect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,30 +2,66 @@
   <div class="container-fluid index">
     <div class="row align-items-center flex-column-reverse flex-md-row">
       <div class="col-12 col-md-5">
-        <div class="card">
-          <div class="card-body">
-            <!-- 該文章資訊 -->
-            <div class="card-info">
-              <span class="badge badge-dark">影音</span>
-              <span class="text-primary">不分癌</span>
-              <span class="text-primary">常見治療問題</span>
-              <span class="text-primary">常見問題</span>
+        <Swiper
+          ref="swiperFade"
+          :modules="modulesFade"
+          :slides-per-view="1"
+          :effect="fade"
+          :fadeEffect="{
+            crossFade: true,
+          }"
+          :grab-cursor="false"
+          :allowTouchMove="false"
+          :loop="true"
+          :autoplay="{
+            delay: 5000,
+            disableOnInteraction: false
+          }"
+          :navigation="{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          }"
+          :pagination="{
+            el: '.swiper-pagination',
+            clickable: true
+          }"
+          @swiper="onSwiper"
+          @slideChange="onSlideChange"
+        >
+          <SwiperSlide v-for="(article, index) in articleData" :key="index">
+            <div class="card">
+              <div class="card-body">
+                <!-- 該文章資訊 -->
+                <div class="card-info">
+                  <span v-if="article.video" class="badge badge-dark">影音</span>
+                  <span
+                    v-for="tag in article.tags"
+                    :key="tag"
+                    class="text-primary">
+                    {{ tag }}
+                  </span>
+                </div>
+                <!-- 該文章標題 -->
+                <p class="card-title">
+                  {{ article.title }}
+                </p>
+                <!-- 立即閱讀 -->
+                <a :href="article.href" class="btn btn-outline-secondary card-link card-link">立即閱讀</a>
+              </div>
             </div>
-            <!-- 該文章標題 -->
-            <p class="card-title">疫情期間，癌症化療病人尿不出來又怕掛急診染疫，怎麼辦？</p>
-            <!-- 瞭解更多 -->
-            <a href="#" class="btn btn-outline-secondary card-link card-link">立即閱讀</a>
-          </div>
-        </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
       <div class="col-12 col-md-7">
         <!-- <img src="https://picsum.photos/800/600" alt="news" title="news"> -->
         <Swiper
+          ref="swiperCard"
           :modules="modules"
           :slides-per-view="1"
           :space-between="50"
           :effect="'cards'"
-          :grab-cursor="true"
+          :grab-cursor="false"
+          :allowTouchMove="false"
           :loop="true"
           :centered-slides="true"
           :autoplay="{
@@ -37,13 +73,16 @@
             prevEl: '.swiper-button-prev'
           }"
           :pagination="{
+            el: '.swiper-pagination',
             clickable: true
           }"
           @swiper="onSwiper"
           @slideChange="onSlideChange"
         >
-          <SwiperSlide v-for="(item, index) in bannerItems" :key="index">
-            <img :src="`https://picsum.photos/768/432?random=${index}`" fluid alt="Banner Image">
+          <SwiperSlide v-for="(article, index) in articleData" :key="index">
+            <router-link :to="article.href" class="text-white">
+              <img :src="`${article.img}?random=${index}`" fluid alt="Banner Image">
+            </router-link>
           </SwiperSlide>
           <div class="swiper-button-prev"></div>
           <div class="swiper-button-next"></div>
@@ -55,7 +94,7 @@
 </template>
 
 <script>
-import { EffectCards, Pagination, Navigation, Autoplay } from 'swiper/modules'
+import { EffectCards, EffectFade, Pagination, Navigation, Autoplay } from 'swiper/modules'
 
 import { Swiper, SwiperSlide } from 'swiper/vue'
 
@@ -65,12 +104,28 @@ import 'swiper/swiper-bundle.css'
 export default {
   data () {
     return {
-      bannerItems: [
-        { title: '標題1', description: '描述1' },
-        { title: '標題2', description: '描述2' },
-        { title: '標題2', description: '描述2' },
-        { title: '標題2', description: '描述2' },
-        { title: '標題3', description: '描述3' }
+      articleData: [
+        {
+          title: '疫情期間，癌症化療病人尿不出來又怕掛急診染疫，怎麼辦？',
+          video: true,
+          tags: ['不分癌', '常見治療問題', '常見問題'],
+          img: 'https://picsum.photos/768/432',
+          href: '/article-1'
+        },
+        {
+          title: '容易暈車怎麼辦？3招有效自救',
+          video: false,
+          tags: ['預防', '暈車'],
+          img: 'https://picsum.photos/768/432',
+          href: '/article-2'
+        },
+        {
+          title: '柑橘防癌、抗發炎，大吉大利養生法一次看',
+          video: true,
+          tags: ['防癌', '抗發炎'],
+          img: 'https://picsum.photos/768/432',
+          href: '/article-3'
+        }
       ]
     }
   },
@@ -78,10 +133,13 @@ export default {
     Swiper,
     SwiperSlide
   },
+  mounted () {
+    // 驗證 $refs 是否正確
+    console.log('$refs', this.$refs)
+    console.log('swiperCard', this.$refs.swiperCard)
+  },
   methods: {
-    // onSlideChange () {
-    //   console.log('Slide changed!')
-    // }
+
   },
   setup () {
     const onSwiper = (swiper) => {
@@ -93,6 +151,7 @@ export default {
     return {
       onSwiper,
       onSlideChange,
+      modulesFade: [EffectFade, Pagination],
       modules: [EffectCards, Pagination, Navigation, Autoplay]
     }
   }
